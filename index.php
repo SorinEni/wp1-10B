17c61ae49f259f8765d6331686cbab22fae0b02f
SorinEni <60381859+SorinEni@users.noreply.github.com>
2020-05-12 10:31:28 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    🦍
+   <?php for ($i=0; $i <= 400; $i+=20) { ?>
+       <div style="display: block; width: <?=$i?>px; height: 20px; background-color: #FF5515;"> </div>
+
+
+
+
+
+
+
+  <?php 
+  
+    } 
+  
+  
+  ?>
+
+   
+
+</body>
+</html>
